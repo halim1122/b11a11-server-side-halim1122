ee0d41a0f8ae2c2930e7034204863dc0be8c2520
Abdul Halim <abdulahlim1100@gmail.com>
2025-06-17 20:30:44 +0600

    added jwt token

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const cors = require('cors');
+var cookieParser = require('cookie-parser')
+var jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
-app.use(cors());
+app.use(cors({
+     origin: ['http://localhost:5173'],
+     credentials: true
+}));
 app.use(express.json())
+app.use(cookieParser())
+
+const logger = (req, res, next) => {
+     console.log("inside the logger")
+     next()
+}
+
+const verifyToken = (req, res, next) =>{
+     const token = req?.cookies?.token;
+     if(!token){
+          return res.status(401).send({message: 'unauthorized access'})
+     }
+     jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
+          if(err){
+               return res.status(401).send({message: 'unauthorized access'})
+          }
+          req.decoded = decoded;
+          next();
+     })
+}
 
 app.get('/', (req, res) => {
      res.send('server is runing')
 })
-
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@abdulhalim.7yzjk6t.mongodb.net/?retryWrites=true&w=majority&appName=AbdulHalim`;
 
@@ -31,8 +55,9 @@ async function run() {
           // await client.connect();
 
           const AssignmentsCollection = client.db('AssignmentsBD').collection('Assignments')
+          const SubmissionsCollection = client.db('AssignmentsBD').collection('Submissions')
 
-          app.get('/assignments', async (req, res) => {
+          app.get('/assignments',verifyToken, async (req, res) => {
                const result = await AssignmentsCollection.find().toArray();
                res.send(result);
           })
@@ -44,23 +69,68 @@ async function run() {
                res.send(result)
           })
 
+          app.get("/submissions", async (req, res) => {
+               try {
+                    const { status, submittedBy } = req.query;
+                    const query = {};
+
+                    if (status) query.status = status;
+                    if (submittedBy) query.submittedBy = submittedBy;
+
+                    const result = await SubmissionsCollection.find(query).toArray();
+                    res.json(result);
+               } catch (error) {
+                    res.status(500).json({ error: "Failed to fetch submissions" });
+               }
+          });
+
+          app.post('/jwt', async (req, res) => {
+               const userInfo = req.body;
+
+               const token = jwt.sign(userInfo, process.env.JWT_ACCESS_SECRET, { expiresIn: '1h' })
+
+               res.cookie('token', token, {
+                    httpOnly: true,
+                    secure: false
+               })
+
+               res.send({ success: true })
+
+          })
+
           app.post('/assignments', async (req, res) => {
                const newAssignment = req.body
                const result = await AssignmentsCollection.insertOne(newAssignment);
                res.send(result);
           })
 
+          app.post("/submissions", async (req, res) => {
+               const submission = req.body;
+               const result = await SubmissionsCollection.insertOne(submission);
+               res.send(result);
+          });
+
           app.put('/assignment/:id', async (req, res) => {
                const id = req.params.id;
                const query = { _id: new ObjectId(id) };
-               const Update =req.body;
+               const Update = req.body;
                const options = { upsert: true };
-               const UpdateDoc ={
+               const UpdateDoc = {
                     $set: Update
                }
                const result = await AssignmentsCollection.updateOne(query, UpdateDoc, options);
                res.send(result);
           })
+
+          app.patch("/submissions/:id", async (req, res) => {
+               const id = req.params.id;
+               const query = { _id: new ObjectId(id) };
+               const updateDoc = {
+                    $set: req.body
+               };
+               const result = await SubmissionsCollection.updateOne(query, updateDoc);
+               res.send(result);
+          });
 
           app.delete('/assignment/:id', async (req, res) => {
                const id = req.params.id;
