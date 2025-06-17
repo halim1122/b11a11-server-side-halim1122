43b54f3de7c82929ab349e4762fa7f1f3a9170bb
Abdul Halim <abdulahlim1100@gmail.com>
2025-06-17 08:22:36 +0600

    full deploy complate to vercel

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 })
 
 
-const uri = `mongodb+srv://${process.env.VITE_NAME}:${process.env.VITE_PASS}@abdulhalim.7yzjk6t.mongodb.net/?retryWrites=true&w=majority&appName=AbdulHalim`;
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@abdulhalim.7yzjk6t.mongodb.net/?retryWrites=true&w=majority&appName=AbdulHalim`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
